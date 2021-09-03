2defcfdfd6ff7d4f72877ef6d1869db13c24969d
sofia durkan <sofiadurkan@gmail.com>
2021-09-03 11:54:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+ 
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  
+  useEffect(() => {
+
+    const storeLoggedinInfo = localStorage.getItem('isLoggedIn');
+
+  if (storeLoggedinInfo === "1"){
+    setIsLoggedIn(true);
+  }
+
+  },[]);
 
   const loginHandler = (email, password) => {
     localStorage.setItem('isLoggedIn','1');
