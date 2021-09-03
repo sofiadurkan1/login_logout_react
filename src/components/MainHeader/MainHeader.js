05af20fbf6175189d9c689ee163d66e29d109a71
sofia durkan <sofiadurkan@gmail.com>
2021-09-03 12:50:55 -0500

    card.css updated

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h1>Login Page</h1>
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
